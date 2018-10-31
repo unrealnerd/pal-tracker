c0ff4b6237a23a20de99a55e53d420f74eb2f6f6
Mike Gehard <mgehard@pivotal.io>
2018-10-31 16:23:59 -0600

    Add manifest file for deployment to PCF

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,17 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton(sp => new WelcomeMessage(
+                Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
+            ));
+
+            services.AddSingleton(sp => new CloudFoundryInfo(
+                Configuration.GetValue<string>("PORT"),
+                Configuration.GetValue<string>("MEMORY_LIMIT"),
+                Configuration.GetValue<string>("CF_INSTANCE_INDEX"),
+                Configuration.GetValue<string>("CF_INSTANCE_ADDR")
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
