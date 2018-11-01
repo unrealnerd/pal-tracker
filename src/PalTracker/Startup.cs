a92046efb8883e5aa86478c735faf62a7b848481
Mike Gehard <mgehard@pivotal.io>
2018-11-01 14:22:28 -0600

    Add TimeEntry MVC in memory

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -37,6 +37,8 @@ namespace PalTracker
                 Configuration.GetValue<string>("CF_INSTANCE_INDEX"),
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR")
             ));
+
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
